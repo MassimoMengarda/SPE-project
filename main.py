65ad5128a50dc330b7348ab1f016f653f95520ec
Matteo Bortolon <m.bortolon.97@gmail.com>
2020-09-28 23:11:25 +0200

    Minimal implementation of the iteration 1, add the basic formula, missing people that are not immunized ratio

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 def main():
-    print("Simulation and Performance Evaluation final project")
+    N = 200
+    max_t = 100
+    beta = 0.152
+    gamma = 0.053
+
+    s = 199/200
+    i = 1/200
+    r = 0.0
+    for t in range(max_t):
+        print("Cycle: {:d}, S: {:d}, I: {:d}, R: {:d}".format(t, int(s * N), int(i * N), int(r * N)))
+        s += - beta * s * i
+        i += beta * s * i - gamma * i
+        r += gamma * i
 
 main()
