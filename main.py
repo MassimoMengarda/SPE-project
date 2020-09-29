c7548f80c256d7ce5c97b8bff1953430260bb882
Matteo Bortolon <m.bortolon.97@gmail.com>
2020-09-29 10:37:40 +0200

    Finish code refactoring

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
+from group import PandemicGroup
+
 def main():
     N = 200
-    max_t = 100
+    I = 1
     beta = 0.152
     gamma = 0.053
+    group = PandemicGroup(N, I, beta, gamma)
 
-    s = 199/200
-    i = 1/200
-    r = 0.0
-    for t in range(max_t):
-        print("Cycle: {:d}, S: {:d}, I: {:d}, R: {:d}".format(t, int(s * N), int(i * N), int(r * N)))
-        s += - beta * s * i
-        i += beta * s * i - gamma * i
-        r += gamma * i
+    max_t = 100
+    PandemicGroup.plot_advancement(group, max_t)
 
 main()
