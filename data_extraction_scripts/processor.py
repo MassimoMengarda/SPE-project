aeb70a9930aab387f3ad5f102ed313af8479ad5f
Massimo Mengarda <max.mengarda@gmail.com>
2021-03-06 12:00:19 +0100

    Created and gitignored data folder, small changes in data ectraction scripts

--- data_extraction_scripts/processor.py
+++ data_extraction_scripts/processor.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def main():
     if len(sys.argv) < 3:
-        sys.exit("Usage: PATH/python3 processor.py <input filename> <output filename> <metro_area_postal_codes>")
+        sys.exit("Usage: PATH/python3 processor.py <input filename> <output filename> <metro area postal codes input filename>")
     
     input_filename = sys.argv[1]
     output_filename = sys.argv[2]
@@ -13,16 +13,11 @@ def main():
     print("Reading csv file ", filtering_postal_codes_filename)
     zip_codes = pd.read_csv(filtering_postal_codes_filename, converters={'zip_code': str})
     
-
     print("Reading csv file", input_filename)
     df = pd.read_csv(input_filename, converters={'postal_code': str})
-    # print(df.describe())
-    # print(df.isna().sum())
 
     print("Filtering file")
     df['postal_code'] = df['postal_code'].apply(lambda x: x.zfill(5))
-    # print(pd.unique(df['postal_code']))
-    # print(pd.unique(df['region']))
     is_metro_area = df['postal_code'].isin(zip_codes['zip_code'])
     df_metro_area = df[is_metro_area]
 
