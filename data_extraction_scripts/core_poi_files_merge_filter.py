9dfa9c1e8c34ae669355b3e8a1191fdff38abf2e
Matteo Bortolon <m.bortolon.97@gmail.com>
2021-03-07 10:56:07 +0100

    Re-add additional data

--- data_extraction_scripts/core_poi_files_merge_filter.py
+++ data_extraction_scripts/core_poi_files_merge_filter.py
@@ -4,11 +4,12 @@ import os
 import pandas as pd
 
 def main():
-    if len(sys.argv) < 2:
-        sys.exit("Usage: PATH/python3 core_poi_files_merge.py <input directory> <output filename>")
+    if len(sys.argv) < 3:
+        sys.exit("Usage: PATH/python3 core_poi_files_merge_filter.py <input directory> <metro area postal codes input filename> <output filename>")
     
     input_directory = sys.argv[1]
-    output_filename = sys.argv[2]
+    zip_codes_filepath = sys.argv[2]
+    output_filename = sys.argv[3]
 
     if not os.path.isdir(input_directory):
         sys.exit(f"Error: {input_directory} is not a valid directory")
@@ -21,12 +22,19 @@ def main():
     dfs = []
     for path in paths:
         print("Reading csv file", path)
-        dfs.append(pd.read_csv(path))
+        dfs.append(pd.read_csv(path, converters={"postal_code": str}))
     
     print("Concatenating files")
     final_df = pd.concat(dfs)
 
-    print(final_df)
+    print("Reading csv file", zip_codes_filepath)
+    zip_codes = pd.read_csv(path, converters={"zip_code": str})
+
+    print("Filtering merge file")
+    final_df["postal_code"] = final_df["postal_code"].apply(lambda x: x.zfill(5))
+
+    is_metro_area = final_df["postal_code"].isin(zip_codes["zip_code"])
+    final_df = final_df[is_metro_area]
 
     print("Writing csv file", output_filename)
     final_df.to_csv(output_filename, index = False)
