8e6f493154087d608cac29b2b7d0519fca6a9ef9
Massimo Mengarda <max.mengarda@gmail.com>
2021-03-12 11:37:45 +0100

    Compute w r matrix size by hand

--- data_extraction_scripts/construct_w_r_matrixes.py
+++ data_extraction_scripts/construct_w_r_matrixes.py
@@ -12,8 +12,16 @@ def main():
     for pattern_file in os.listdir(dir1):
         df = pd.read_csv("/weekly_pattern_link.csv", converters={'postal_code':str,'visitor_home_cbgs':JSONParser})
 
-        df["raw_visitor_counts"]  df["visitor_home_cbgs"]
+        df["raw_visitor_counts"] / df["visitor_home_cbgs"]
         print(df)
+
+        # row => POI (315'000)
+        # column => CBG (12'000)
+        # Around => 3'780'000'000 entries
+        # Consequently => sparse matrix SciPy
+        # Entry => 4 byte
+        # Total => ~16GB
+        
 
 
 if __name__ == "__main__":
