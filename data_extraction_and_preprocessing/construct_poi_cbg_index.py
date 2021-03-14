0004491bebd1569a887c3c9752505585246c5bd8
Massimo Mengarda <max.mengarda@gmail.com>
2021-03-14 14:41:02 +0100

    Modify matrix indexes scripts

--- data_extraction_and_preprocessing/construct_poi_cbg_index.py
+++ data_extraction_and_preprocessing/construct_poi_cbg_index.py
@@ -1,53 +1,67 @@
-import os
-import pandas as pd
-import json
-import numpy as np
 import argparse
+import json
+import os
 import sys
-import scipy
+
+import numpy as np
+import pandas as pd
+
+def JSONParser(data):
+    return json.loads(data)
 
 def main():
     parser = argparse.ArgumentParser(description="Construct the w(r) matrixes, one for each week considered")
     parser.add_argument("input_directory", type=str, help="the directory where the weekly patterns are stored")
-    parser.add_argument("output_filepath", type=str, help="the file where store the index")
+    parser.add_argument("output_directory", type=str, help="the directory where store the index result")
     args = parser.parse_args()
     input_dir = args.input_directory
-    output_filepath = args.output_filepath
+    output_directory = args.output_directory
 
     if not os.path.isdir(input_dir):
         print("Input directory is not a directory")
         sys.exit(1)
     
-    pattern_files = [path for path in os.listdir(input_dir) if path.endswith(".csv")]
+    pattern_files = [os.path.join(input_dir, path) for path in os.listdir(input_dir) if path.endswith(".csv")]
 
     if len(pattern_files) == 0:
         print("Given input directory do not contain any CSV file") 
         sys.exit(1)
 
-    safe_graph_place_ids = None
+    os.makedirs(output_directory, exist_ok=True)
+
+    safe_graph_place_ids_not_set = True
+    cbgs = []
     for pattern_file in pattern_files:
-        df = pd.read_csv(pattern_file)
+        print("Reading CSV file", pattern_file)
+        df = pd.read_csv(pattern_file, converters={"visitor_home_cbgs": JSONParser})
         
-        if safe_graph_place_ids == None:
+        if safe_graph_place_ids_not_set:
             safe_graph_place_ids = df["safegraph_place_id"]
+            safe_graph_place_ids_not_set = False
         else:
             safe_graph_place_ids.append(df["safegraph_place_id"])
+        
+        for i, row in df.iterrows():
+            cbgs.extend(row["visitor_home_cbgs"].keys())
     
     pois_idx = pd.unique(safe_graph_place_ids)
     matrix_positions = np.arange(0, len(pois_idx))
+    result_poi_mapping = pd.DataFrame(data={"matrix_idx": matrix_positions, "poi": pois_idx})
+    result_poi_mapping.to_csv(os.path.join(output_directory, "poi_indexes.csv"), index=False)
 
-    result_poi_mapping = pd.DataFrame(data={'matrix_idx':matrix_positions,'poi_idx': pois_idx})
-
-    result_poi_mapping.to_csv(output_filepath)
-
-        # row => POI (315'000)
-        # column => CBG (12'000)
-        # Around => 3'780'000'000 entries
-        # Consequently => sparse matrix SciPy
-        # Entry => 4 byte
-        # Total => ~16GB
-        
-
+    unique_cbgs = pd.unique(cbgs)
+    matrix_positions = np.arange(0, len(unique_cbgs))
+    result_cbg_mapping = pd.DataFrame(data={"matrix_idx": matrix_positions, "cbg": unique_cbgs})
+    result_cbg_mapping.to_csv(os.path.join(output_directory, "cbg_indexes.csv"), index=False)
 
 if __name__ == "__main__":
     main()
+
+# row => POI (315'000)
+# column => CBG (12'000)
+# Around => 3'780'000'000 entries
+# Consequently => sparse matrix SciPy
+# Entry => 4 byte
+# Total => ~16GB
+
+"{""360810040021"":7,""360810086001"":5,""360810020001"":5,""360850011003"":4,""360810178002"":4,""360810174001"":4,""360810942031"":4,""360810036002"":4,""360810098002"":4,""360810184022"":4,""360810062023"":4}"
--- data_extraction_and_preprocessing/construct_w_r_matrixes.py
+++ data_extraction_and_preprocessing/construct_w_r_matrixes.py
@@ -5,7 +5,7 @@ import sys
 
 import numpy as np
 import pandas as pd
-import scipy
+from scipy.sparse import coo_matrix, save_npz
 
 def JSONParser(data):
     j1 = json.loads(data)
@@ -14,41 +14,102 @@ def JSONParser(data):
 def main():
     parser = argparse.ArgumentParser(description="Construct the w(r) matrixes, one for each week considered")
     parser.add_argument("input_directory", type=str, help="the directory where the weekly patterns are stored")
+    parser.add_argument("index_directory", type=str, help="the directory where the matrix index are stored")
     parser.add_argument("output_directory", type=str, help="the directory where save the matrixes elaborated")
     args = parser.parse_args()
     input_dir = args.input_directory
+    index_dir = args.index_directory
     output_dir = args.output_directory
 
     if not os.path.isdir(input_dir):
         print("Input directory is not a directory")
         sys.exit(1)
     
-    os.makedirs(output_dir)
+    poi_idx_filename = os.path.join(index_dir, "poi_indexes.csv")
+    cbg_idx_filename = os.path.join(index_dir, "cbg_indexes.csv")
+
+    if (not os.path.isfile(poi_idx_filename)) or (not os.path.isfile(cbg_idx_filename)):
+        print("The given indexes directory do not contain the valid index files")
+        sys.exit(1)
     
-    pattern_files = [path for path in os.listdir(input_dir) if path.endswith(".csv")]
+    os.makedirs(output_dir, exist_ok=True)
+
+    poi_idx_file = pd.read_csv(poi_idx_filename)
+    poi_idx_file.rename(columns={"matrix_idx": "poi_matrix_idx"}, inplace=True)
+    cbg_idx_file = pd.read_csv(cbg_idx_filename, converters={"cbg": str})
+    cbg_idx_file.rename(columns={"matrix_idx": "cbg_matrix_idx"}, inplace=True)
+    
+    pattern_files = [(path, os.path.join(input_dir, path)) for path in os.listdir(input_dir) if path.endswith(".csv")]
 
     if len(pattern_files) == 0:
         print("Given input directory do not contain any CSV file") 
         sys.exit(1)
 
-
-    for pattern_file in pattern_files:
-        df = pd.read_csv(pattern_file, converters={'postal_code':str,'visitor_home_cbgs':JSONParser})
-        sum_poi_cbg = [sum(df["visitor_home_cbgs"][i].values()) for x in df['col']]
+    for filename, pattern_file in pattern_files:
+        print("Reading CSV file ", pattern_file)
+        df = pd.read_csv(pattern_file, converters={"postal_code": str, "visitor_home_cbgs": JSONParser})
+        
+        print("Computing ratio")
+        sum_poi_cbg = [sum(x.values()) for x in df["visitor_home_cbgs"]]
         
         sum_poi_cbg = np.asarray(sum_poi_cbg)
-        ratio = df["raw_visitor_counts"][i] / sum_poi_cbg
+        ratio = df["raw_visitor_counts"] / sum_poi_cbg
+
+        print("Computing weights")
 
         data = []
         pois = []
         cbgs = []
 
         for idx, poi in df.iterrows():
-            data.extend(poi["visitor_home_cbgs"].values() / sum_poi_cbg[idx] * ratio[idx])
-            poi_idx.extend([idx for i in len(poi["visitor_home_cbgs"])])
+            data.extend(list(poi["visitor_home_cbgs"].values()) / sum_poi_cbg[idx] * ratio[idx])
+            pois.extend([poi["safegraph_place_id"] for i in range(len(poi["visitor_home_cbgs"]))])
             cbgs.extend(poi["visitor_home_cbgs"].keys())
         
-        print(df)
+        print("Computing POI indexes")
+
+        weights_df = pd.DataFrame(data={'data': data, 'poi': pois, 'cbg': cbgs})
+        result_poi_merge = pd.merge(weights_df, poi_idx_file, on="poi")
+
+        print("Computing CBG indexes")
+        result_merge = pd.merge(result_poi_merge, cbg_idx_file, on="cbg")
+        
+        # print(result_merge)
+
+        
+        # poi_idxs = pois_series.apply(lambda poi: poi_idx_file['matrix_idx'][poi_idx_file['poi'] == poi].values[0])
+        
+        # poi_idxs = []
+        # for poi in pois:
+        #     poi_idxs.append(poi_idx_file.loc[poi_idx_file["poi"] == poi].iloc[0]["matrix_idx"])
+        # print(poi_idxs)
+
+        # print("Computing CBG indexes")
+# 
+        # cbg_idxs = []
+        # for cbg in cbgs:
+        #     cbg_idxs.append(cbg_idx_file.loc[cbg_idx_file["cbg"] == cbg].iloc[0]["matrix_idx"])
+        
+        # print("Creating sparse matrix")
+
+        coo_init_value = (result_merge['data'], (result_merge['poi_matrix_idx'], result_merge['cbg_matrix_idx']))
+        coo_shape = (poi_idx_file['poi_matrix_idx'].max() + 1, cbg_idx_file['cbg_matrix_idx'].max() + 1)
+        
+        # print((result_merge['poi_matrix_idx'] >= coo_shape[0]).sum())
+        # print((result_merge['poi_matrix_idx'] > coo_shape[0]).sum())
+        # print(result_merge['poi_matrix_idx'].isnull().sum())
+        # print((result_merge['cbg_matrix_idx'] >= coo_shape[1]).sum())
+        # print((result_merge['cbg_matrix_idx'] > coo_shape[1]).sum())
+        # print(result_merge['cbg_matrix_idx'].isnull().sum())
+# 
+        # print(coo_shape)
+        
+        w_r_sparse_matrix = coo_matrix(coo_init_value, shape=coo_shape)
+        output_filepath = os.path.join(output_dir, (os.path.splitext(filename)[0] + ".npz"))
+        print("Writing file ", output_filepath)
+        save_npz(output_filepath, w_r_sparse_matrix)
+
+
 
 if __name__ == "__main__":
     main()
