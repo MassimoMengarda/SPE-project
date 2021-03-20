cad6ffec2f7e6932f15216b9e7328d0a55761cef
Massimo Mengarda <max.mengarda@gmail.com>
2021-03-20 18:37:05 +0100

    Add extension to utils.get_dates_from_input_dir

--- data_extraction_and_preprocessing/utils.py
+++ data_extraction_and_preprocessing/utils.py
@@ -2,13 +2,13 @@ import json
 import os
 from datetime import datetime
 
-def get_dates_from_input_dir(input_dir):
+def get_dates_from_input_dir(input_dir, extension=".csv"):
     result = []
 
     for filename in os.listdir(input_dir):
-        if filename.endswith(".csv"):
+        if filename.endswith(extension):
             filename_date = datetime.strptime(filename[:len("2019-01-08")], "%Y-%m-%d")
-            new_filename = filename_date.strftime("%Y-%m-%d") + ".csv"
+            new_filename = filename_date.strftime("%Y-%m-%d") + extension
             result.append((new_filename, os.path.join(input_dir, filename)))
 
     return result
