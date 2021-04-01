2ea5af3ce2b968989080f4b30170d4deac06c4fb
Massimo Mengarda <max.mengarda@gmail.com>
2021-04-01 13:30:56 +0200

    Fix construct IPFP

--- data_extraction_and_preprocessing/construct_w_r_ipfp.py
+++ data_extraction_and_preprocessing/construct_w_r_ipfp.py
@@ -19,10 +19,14 @@ def ipfp_hour(aggregate_visits_w, cbg_marginals_u, poi_marginal_v, number_of_ite
     for tau in range(1, number_of_iterations):
         new_w = None
         if tau % 2 == 1: # if tau is odd
-            alfa_i = cbg_marginals_u / last_w.sum(axis=0).ravel()
+            last_w_axis_sum = last_w.sum(axis=0)
+            last_w_axis_sum[last_w_axis_sum < sys.float_info.epsilon] = 1.0
+            alfa_i = cbg_marginals_u / last_w_axis_sum
             new_w = last_w.multiply(alfa_i)                
         else:
-            last_w_coo = coo_matrix(last_w.sum(axis=1))
+            last_w_axis_sum = last_w.sum(axis=1)
+            last_w_axis_sum[last_w_axis_sum < sys.float_info.epsilon] = 1.0
+            last_w_coo = coo_matrix(last_w_axis_sum)
             alfa_j = poi_marginal_v / last_w_coo
             new_w = last_w.multiply(alfa_j)
         last_w = new_w
@@ -31,11 +35,11 @@ def ipfp_hour(aggregate_visits_w, cbg_marginals_u, poi_marginal_v, number_of_ite
 def ipfp(day, hour, aggregate_visits_w, cbg_marginals_u, poi_marginal_v, day_dir):
     print(f"Computing IPFP {day} - hour {hour}")
     w_sparse_matrix = ipfp_hour(aggregate_visits_w, cbg_marginals_u, poi_marginal_v, number_of_iterations=100)
-    w_matrix_filename = os.path.join(day_dir, "{:0>2d}.npz".format(hour))
+    w_matrix_filename = os.path.join(day_dir, "{:0>3d}.npz".format(hour))
     print("Saving NPZ", w_matrix_filename)
     save_npz(w_matrix_filename, w_sparse_matrix)
 
-def main(aggregate_visit_matrix_dir, cbg_marginals_dir, poi_marginals_dir, output_dir):    
+def main(aggregate_visit_matrix_dir, cbg_marginals_dir, poi_marginals_dir, output_dir, no_weeks=None):    
     aggregate_visits_w = read_npz(os.path.join(aggregate_visit_matrix_dir, "aggregate_visit_matrix.npz"))
     cbg_marginals_files = get_dates_from_input_dir(cbg_marginals_dir, extension=".npy")
     poi_marginals_files = get_dates_from_input_dir(poi_marginals_dir, extension=".npz")
@@ -46,6 +50,10 @@ def main(aggregate_visit_matrix_dir, cbg_marginals_dir, poi_marginals_dir, outpu
 
     for (filename, cbg_marginal_filepath), (_, poi_marginal_filepath) in zip(cbg_marginals_files, poi_marginals_files):
         day = datetime.strptime(filename[:len("2019-01-08")], "%Y-%m-%d").strftime("%Y-%m-%d")
+        if not no_weeks is None and day in no_weeks:
+            print(f"Skipping {day}")
+            continue
+
         day_dir = os.path.join(output_dir, day)
         os.makedirs(day_dir, exist_ok=True)
         cbg_marginals_u_t = read_npy(cbg_marginal_filepath)
@@ -61,10 +69,12 @@ if __name__ == "__main__":
     parser.add_argument("cbg_marginals_directory", type=str, help="the directory where the cbg marginals are stored")
     parser.add_argument("poi_marginals_directory", type=str, help="the directory where the poi marginals are stored")
     parser.add_argument("output_directory", type=str, help="the directory where save the results")
+    parser.add_argument("-nw", "--no_weeks", nargs="*", help="specify the weeks you want to exclude", default=None)
     args = parser.parse_args()
     aggregate_visit_matrix_dir = args.aggregate_visit_matrix_directory
     cbg_marginals_dir = args.cbg_marginals_directory
     poi_marginals_dir = args.poi_marginals_directory
     output_dir = args.output_directory
+    no_weeks = args.no_weeks
 
-    main(aggregate_visit_matrix_dir, cbg_marginals_dir, poi_marginals_dir, output_dir)
+    main(aggregate_visit_matrix_dir, cbg_marginals_dir, poi_marginals_dir, output_dir, no_weeks)
