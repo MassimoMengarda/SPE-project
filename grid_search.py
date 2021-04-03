6cadb3755c89c3adaed638c231b0a78b924edc05
Matteo Bortolon <m.bortolon.97@gmail.com>
2021-04-03 11:48:30 +0200

    Gird search modify for multiple batch

--- grid_search.py
+++ grid_search.py
@@ -34,49 +34,47 @@ def main(info_dir, ipfp_dir, dwell_dir, cases_filepath, output_dir):
     
     simulation_start = datetime.datetime(2020, 3, 2, 0)
     simulation_end = datetime.datetime(2020, 5, 10, 23)
+    batch = 10
 
     output_path = os.path.join(output_dir, "rmse_result.csv")
                     
     if not os.path.isfile(output_path):
         with open(output_path, "a") as f_handle:
-            f_handle.write("b_base;psi;p_0;i;rmse\n")
+            f_handle.write("b_base;psi;p_0;rmse\n")
 
     for b_base in b_base_s:
         for psi in psi_s:
             for p_0 in p_0_s:
                 print(f"Computing parameters b_base {b_base} psi {psi} p_0 {p_0}")
                 day_new_cases = [0 for _ in range(24 + delta_c)]
-                rmse_s = []
-                for i in range(n_simulation):
-                    print(f"Running simulation number {i + 1}")
-                    rmse_sum = 0
+                
+                print(f"Running simulation number {i + 1}")
+                rmse_sum = np.zeros((batch, 1), dtype=np.float32)
 
-                    m = Model(cbgs_population, ipfp_dir, dwell_dir, None, n_pois, pois_area, b_base, psi, p_0, t_e=96, t_i=84)
+                m = Model(cbgs_population, ipfp_dir, dwell_dir, None, n_pois, pois_area, b_base, psi, p_0, t_e=96, t_i=84, batch=batch)
 
-                    days_of_simulation = 0
-                    for simulation_time, week_string, week_t, cbg_s, cbg_e, cbg_i, cbg_r_dead, cbg_r_alive, cbg_new_i in m.simulate(simulation_start, simulation_end):
-                        day_new_cases.append(np.sum(cbg_new_i))
-                        day_new_cases.pop(0)
-                        
-                        if simulation_time.hour == 0:
-                            cases_index = simulation_time - datetime.timedelta(days=1)
-                            if cases_index in cases.index:
-                                certified_new_cases = cases.loc[cases_index]["cases"]
-                            else:
-                                certified_new_cases = 0
-                            estimated_confirmed_new_cases = confirmed_new_cases_proportion * np.sum(day_new_cases[0:24])
-                            rmse_sum += (estimated_confirmed_new_cases - certified_new_cases) ** 2
-                            days_of_simulation += 1
+                days_of_simulation = 0
+                for simulation_time, week_string, week_t, cbg_s, cbg_e, cbg_i, cbg_r_dead, cbg_r_alive, cbg_new_i in m.simulate(simulation_start, simulation_end):
+                    day_new_cases.pop(0)
+                    day_new_cases.append(np.sum(cbg_new_i, axis=2)) # TODO check size (10, 1, 140k)
                     
-                    rmse = math.sqrt(rmse_sum / days_of_simulation)
-                    rmse_s.append(rmse)
-
-                    with open(output_path, "a") as f_handle:
-                        f_handle.write("{};{};{};{};{}\n".format(b_base, psi, p_0, i, rmse))
-                        f_handle.flush()
-
-                average_rmse = sum(rmse_s) / len(rmse_s)
+                    if simulation_time.hour == 0:
+                        cases_index = simulation_time - datetime.timedelta(days=1)
+                        if cases_index in cases.index:
+                            certified_new_cases = cases.loc[cases_index]["cases"]
+                        else:
+                            certified_new_cases = 0
+                        estimated_confirmed_new_cases = confirmed_new_cases_proportion * np.sum(day_new_cases[0:24], axis=1)
+                        rmse_sum = rmse_sum + (estimated_confirmed_new_cases - certified_new_cases) ** 2 
+                        days_of_simulation += 1
+                
+                rmse = np.sqrt(rmse_sum / days_of_simulation)
+                average_rmse = np.mean(rmse) # sum(rmse_sum) / rmse_sum.shape[0]
                 print(f"rmse {average_rmse} with b_base {b_base}, psi {psi} p_0 {p_0}")
+
+                with open(output_path, "a") as f_handle:
+                    f_handle.write("{};{};{};{}\n".format(b_base, psi, p_0, average_rmse))
+                    f_handle.flush()
 
                 if (best_params[0] > average_rmse):
                     best_params = (average_rmse, b_base, psi, p_0)
