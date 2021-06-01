b317ec668709c658f3445f627b2127c986175dc2
Massimo Mengarda <max.mengarda@gmail.com>
2021-06-01 18:32:49 +0200

    Fix filter_gdp_counties and scale_io_matrix

--- data_extraction_and_preprocessing/economics/filter_gdp_counties.py
+++ data_extraction_and_preprocessing/economics/filter_gdp_counties.py
@@ -1,8 +1,7 @@
 import argparse
-import os
+import json
 
 import numpy as np
-import pandas as pd
 
 from ..utils import read_csv
 
@@ -10,66 +9,36 @@ def main(gmp_county_filepath, county_list_filepath, output_filepath):
     gmp_county_file = read_csv(gmp_county_filepath)
     counties_file = read_csv(county_list_filepath)
     
+    print("Computing NY area counties")
     mask = np.full((len(gmp_county_file),), False)
     for idx, gmp_county in gmp_county_file.iterrows():
         for _, counties in counties_file.iterrows():
-            gmp_county["county"] == counties[""]
-            if 
-            mask[idx] = True
+            if gmp_county["county"].strip().lower() == counties["name"].strip().lower():
+                mask[idx] = True
     
-    gmp_county_file = gmp_county_file[mask]
-    
-    
-    
-    
-    categories = pd.Series(pd.unique(poi_categories_file["sub_category"]))
-    categories_df = pd.DataFrame({"cat_name": categories, "lower_cat_name": categories.str.lower().str.strip()})
-    
-    categories_df = categories_df[~(pd.isna(categories_df["lower_cat_name"]))]
-    
-    naics_df = read_csv(naics_code_list_path, sep=";", converters={'code': str})
-    naics_df["category"] = naics_df["category"].str.lower().str.strip()
-    categories_with_naics_code_df = categories_df.merge(naics_df, left_on="lower_cat_name", right_on="category", how="left")
-    # Add missing element
-    categories_with_naics_code_df.loc[categories_with_naics_code_df["cat_name"].str.strip() == "Malls"] = ("Malls", "malls", "531120", "Lessors of Nonresidential Buildings (except Miniwarehouses)", "both")
-    io_sector_list = []
-    io_df = read_csv(naics_to_io_sector_path, sep=";", converters={'NAICS': str,'sector_number':int})
-    for _, categories_with_naics_code_row in categories_with_naics_code_df.iterrows():
-        naics_code = categories_with_naics_code_row["code"]
-        is_best_fitting_set = False
-        best_fitting = -1
-        best_fitting_eq_chars = 0
-        for _, io_df_row in io_df.iterrows():
-            if io_df_row["NAICS"].strip() == "NA":
-                continue
-            
-            io_naics_codes = [x.strip() for x in io_df_row["NAICS"].split(",")]
-            for io_naics_code in io_naics_codes:
-                if not is_best_fitting_set:
-                    is_best_fitting_set = True
-                    best_fitting = io_df_row["sector_number"]
-                    best_fitting_eq_chars = equal_characters(naics_code, io_naics_code)
-                    continue
-                
-                current_eq_char = equal_characters(naics_code, io_naics_code)
-                if current_eq_char > best_fitting_eq_chars:
-                    best_fitting_eq_chars = current_eq_char
-                    best_fitting = io_df_row["sector_number"]
-        io_sector_list.append(best_fitting)
-    
-    categories_with_io_df = categories_with_naics_code_df.join(pd.DataFrame({"io_sector": io_sector_list}))
+    gmp_county_file_clean = gmp_county_file[mask]
 
-    poi_categories_with_io_df = poi_categories_file.merge(categories_with_io_df, left_on="sub_category", right_on="cat_name", how="left")
+    total_gdp = gmp_county_file[gmp_county_file["county"] == "United States"].loc[0]["gdp_2019"]
+    ny_are_gdp = gmp_county_file_clean["gdp_2019"].sum()
 
-    output_filepath = os.path.join(info_dir, "poi_categories_with_io_sector.csv")
-    print("Writing file ", output_filepath)
-    poi_categories_with_io_df.to_csv(output_filepath)
+    print(total_gdp)
+    print(ny_are_gdp)
+
+    scale_factor = ny_are_gdp / total_gdp
+
+    print(scale_factor)
+
+    print("Writing JSON ", output_filepath)
+    with open(output_filepath, 'w') as fhandle:
+        json.dump({"metro_area_scale_factor" : scale_factor}, fhandle)
+    
+    # gmp_county_file_clean.to_csv(output_filepath, index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Filter the GMP according to the list of counties in the simulation")
     parser.add_argument("gmp_county_filepath", type=str, help="the path to the file containing the gmp counties data")
     parser.add_argument("county_list_filepath", type=str, help="the path to the file containing the list of counties is stored")
-    parser.add_argument("output_filepath", type=str, help="the path where to save the resul")
+    parser.add_argument("output_filepath", type=str, help="the path where to save the results as json")
     
     args = parser.parse_args()
     gmp_county_filepath = args.gmp_county_filepath
--- data_extraction_and_preprocessing/economics/scale_io_matrix.py
+++ data_extraction_and_preprocessing/economics/scale_io_matrix.py
@@ -1,74 +1,38 @@
 import argparse
+import json
 import os
-
-import pandas as pd
+import sys
 
 from ..utils import read_csv
 
-def equal_characters(str1, str2):
-    eq_char = 0
-    i = 0
-    while i < min(len(str1), len(str2)):
-        if str1[i] == str2[i]:
-            eq_char += 1
-        else:
-            break
-        i += 1
-    return eq_char
-
-def main(info_dir, naics_code_list_path, naics_to_io_sector_path):
-    poi_categories_file = read_csv(os.path.join(info_dir, "poi_categories.csv"))
-    categories = pd.Series(pd.unique(poi_categories_file["sub_category"]))
-    categories_df = pd.DataFrame({"cat_name": categories, "lower_cat_name": categories.str.lower().str.strip()})
+def main(io_matrix_dir, scale_factor_filepath, output_dir):
+    print("Reading file", scale_factor_filepath)
+    with open(scale_factor_filepath, 'r') as fhandle:
+        scale_factor = json.load(fhandle)["metro_area_scale_factor"]
     
-    categories_df = categories_df[~(pd.isna(categories_df["lower_cat_name"]))]
-    
-    naics_df = read_csv(naics_code_list_path, sep=";", converters={'code': str})
-    naics_df["category"] = naics_df["category"].str.lower().str.strip()
-    categories_with_naics_code_df = categories_df.merge(naics_df, left_on="lower_cat_name", right_on="category", how="left")
-    # Add missing element
-    categories_with_naics_code_df.loc[categories_with_naics_code_df["cat_name"].str.strip() == "Malls"] = ("Malls", "malls", "531120", "Lessors of Nonresidential Buildings (except Miniwarehouses)", "both")
-    io_sector_list = []
-    io_df = read_csv(naics_to_io_sector_path, sep=";", converters={'NAICS': str,'sector_number':int})
-    for _, categories_with_naics_code_row in categories_with_naics_code_df.iterrows():
-        naics_code = categories_with_naics_code_row["code"]
-        is_best_fitting_set = False
-        best_fitting = -1
-        best_fitting_eq_chars = 0
-        for _, io_df_row in io_df.iterrows():
-            if io_df_row["NAICS"].strip() == "NA":
-                continue
-            
-            io_naics_codes = [x.strip() for x in io_df_row["NAICS"].split(",")]
-            for io_naics_code in io_naics_codes:
-                if not is_best_fitting_set:
-                    is_best_fitting_set = True
-                    best_fitting = io_df_row["sector_number"]
-                    best_fitting_eq_chars = equal_characters(naics_code, io_naics_code)
-                    continue
-                
-                current_eq_char = equal_characters(naics_code, io_naics_code)
-                if current_eq_char > best_fitting_eq_chars:
-                    best_fitting_eq_chars = current_eq_char
-                    best_fitting = io_df_row["sector_number"]
-        io_sector_list.append(best_fitting)
-    
-    categories_with_io_df = categories_with_naics_code_df.join(pd.DataFrame({"io_sector": io_sector_list}))
+    if not os.path.isdir(io_matrix_dir):
+        sys.exit(io_matrix_dir + " is not a valid directory")
 
-    poi_categories_with_io_df = poi_categories_file.merge(categories_with_io_df, left_on="sub_category", right_on="cat_name", how="left")
+    os.makedirs(output_dir, exist_ok=True)
+    
+    for io_matrix in os.listdir(io_matrix_dir):
+        io_matrix_path = os.path.join(io_matrix_dir, io_matrix)
+        io_matrix_df = read_csv(io_matrix_path, index_col="Sector")
+        io_matrix_df_scaled = io_matrix_df * scale_factor
 
-    output_filepath = os.path.join(info_dir, "poi_categories_with_io_sector.csv")
-    print("Writing file ", output_filepath)
-    poi_categories_with_io_df.to_csv(output_filepath)
+        scaled_output_filepath = os.path.join(output_dir, io_matrix)
+
+        print("Writing file ", scaled_output_filepath)
+        io_matrix_df_scaled.to_csv(scaled_output_filepath)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Construct the IO matrixes strating from POI categories and NAICS codes")
-    parser.add_argument("info_directory", type=str, help="the directory where the poi categories file is stored")
-    parser.add_argument("naics_code_list_path", type=str, help="the path to the file containing the list of NAICS codes")
-    parser.add_argument("naics_to_io_sector_path", type=str, help="the path to the file containing the binding between IO sectors and NAICS codes")
+    parser.add_argument("io_matrix_dir", type=str, help="the directory containing the economics IO matrixes")
+    parser.add_argument("scale_factor_filepath", type=str, help="the path to the file containing the scale factor of the IO matrix")
+    parser.add_argument("output_dir", type=str, help="the directory where to save the results")
     
     args = parser.parse_args()
-    info_dir = args.info_directory
-    naics_code_list_path = args.naics_code_list_path
-    naics_to_io_sector_path = args.naics_to_io_sector_path
-    main(info_dir, naics_code_list_path, naics_to_io_sector_path)
+    io_matrix_dir = args.io_matrix_dir
+    scale_factor_filepath = args.scale_factor_filepath
+    output_dir = args.output_dir
+    main(io_matrix_dir, scale_factor_filepath, output_dir)
