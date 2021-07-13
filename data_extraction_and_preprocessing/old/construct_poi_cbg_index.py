b0dce381ef09b18db8a9feea4c5d8d31e6c135ef
Matteo Bortolon <m.bortolon.97@gmail.com>
2021-07-13 16:31:32 +0200

    Correct error in creating index

--- data_extraction_and_preprocessing/old/construct_poi_cbg_index.py
+++ data_extraction_and_preprocessing/old/construct_poi_cbg_index.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 from utils import get_dates_from_input_dir, JSONParser, read_csv
 
-def main(input_directory, zip_cbg_filepath, output_directory):
+def main(input_directory, output_directory):
     pattern_files = get_dates_from_input_dir(input_directory)
     os.makedirs(output_directory, exist_ok=True)
 
@@ -31,9 +31,8 @@ def main(input_directory, zip_cbg_filepath, output_directory):
     output_filepath = os.path.join(output_directory, "poi_indexes.csv")
     print("Writing csv file", output_filepath)
     result_poi_mapping.to_csv(output_filepath, index=False)
-
-    zip_cbg_bind_df = read_csv(zip_cbg_filepath, converters={'cbg': str, 'zip': str})
-    unique_cbgs = pd.unique(zip_cbg_bind_df["cbg"])
+    
+    unique_cbgs = pd.unique(cbgs)
     matrix_positions = np.arange(0, len(unique_cbgs))
     result_cbg_mapping = pd.DataFrame(data={"matrix_idx": matrix_positions, "cbg": unique_cbgs})
 
@@ -44,12 +43,9 @@ def main(input_directory, zip_cbg_filepath, output_directory):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Construct the poi cbg index matrixes")
     parser.add_argument("input_directory", type=str, help="the directory where the weekly patterns are stored")
-    parser.add_argument("zip_cbg_filepath", type=str, help="the directory where the bind between CBG and ZIP list")
     parser.add_argument("output_directory", type=str, help="the directory where store the index result")
-    
     args = parser.parse_args()
     input_directory = args.input_directory
-    zip_cbg_filepath = args.zip_cbg_filepath
     output_directory = args.output_directory
 
-    main(input_directory, zip_cbg_filepath, output_directory)
+    main(input_directory, output_directory)
